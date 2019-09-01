33ba2cff809147de580318135f3f1b4388e78bbe
Erika Kuntar <erikakuntar@gmail.com>
2019-09-01 11:35:47 -0700

    can now create a new listing

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -4,4 +4,26 @@ class ListingsController < ApplicationController
     @product = Product.find(params[:product_id])
     @listings = @product.listings
   end
+
+  def new
+    @product = Product.find(params[:product_id])
+    @listing = @product.listings.new
+  end
+
+  def create
+    @product = Product.find(params[:product_id])
+    @listing = @product.listings.new(listing_params)
+    if @listing.save
+      redirect_to product_listings_path(@product)
+    else
+      render :new
+    end
+  end
+
+private
+
+  def listing_params
+    params.require(:listing).permit(:model, :year, :color)
+  end
+
 end
