9be12a94d5aea7b8cef9fbe62105c48ada998b5d
Erika Kuntar <erikakuntar@gmail.com>
2019-08-31 23:32:55 -0700

    product can now be edited

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,4 +7,15 @@ class ProductsController < ApplicationController
     @product = Product.find(params[:id])
   end
 
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    product_params = params.require(:product).permit(:name)
+    @product.update(product_params)
+    redirect_to @product
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   root "products#index"
   get "products" => "products#index"
   get "products/:id" => "products#show", as: "product"
+  get "products/:id/edit" => "products#edit", as: "edit_product"
+  patch "products/:id" => "products#update"
 end
