209ca624b7e004b0a91f548fc6afed655ace96e6
Erika Kuntar <erikakuntar@gmail.com>
2019-09-01 00:02:23 -0700

    can now create new products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -13,9 +13,24 @@ class ProductsController < ApplicationController
 
   def update
     @product = Product.find(params[:id])
-    product_params = params.require(:product).permit(:name)
     @product.update(product_params)
     redirect_to @product
   end
 
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    @product.save
+    redirect_to @product
+  end
+
 end
+
+private
+
+  def product_params
+    params.require(:product).permit(:name)
+  end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   root "products#index"
-  get "products" => "products#index"
-  get "products/:id" => "products#show", as: "product"
-  get "products/:id/edit" => "products#edit", as: "edit_product"
-  patch "products/:id" => "products#update"
+  # get "products" => "products#index"
+  # get "products/:id" => "products#show", as: "product"
+  # get "products/:id/edit" => "products#edit", as: "edit_product"
+  # patch "products/:id" => "products#update"
+  resources :products
 end
